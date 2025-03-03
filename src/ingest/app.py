aabba9ab7ee939d200a16dbff4c845047be0c4a6
Vuong Ho <hochivuong2002@gmail.com>
2025-03-03 12:30:57 -0500

    Add drop down

--- src/ingest/app.py
+++ src/ingest/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import networkx as nx
-from pyvis.network import Network
+from st_link_analysis import st_link_analysis, NodeStyle, EdgeStyle
 import fitz  # PyMuPDF
 import tempfile
 import os
@@ -58,16 +58,58 @@ st.markdown("<h1 style='text-align: center;'>Project Overview Dashboard</h1>", u
 
 is_ready = True
 
-if not os.environ["OPENAI_API_KEY"] .startswith("sk-"):
-    st.warning("Please enter your OpenAI API key for interactive GraphRAG modification!", icon="⚠")
-    is_ready = False
+# if not os.environ["OPENAI_API_KEY"] .startswith("sk-"):
+#     st.warning("Please enter your OpenAI API key for interactive GraphRAG modification!", icon="⚠")
+#     is_ready = False
 
-if not uploaded_files:
-    st.warning("Upload your project-related documents here for processing into graph database.", icon="📄")
-    is_ready = False
+# if not uploaded_files:
+#     st.warning("Upload your project-related documents here for processing into graph database.", icon="📄")
+#     is_ready = False
 
 if not is_ready:
     st.stop()
 
-st.markdown("<h1 style='text-align: center;'>Project Overview Dashboard</h1>", unsafe_allow_html=True)
-st.write("hello") 
+def employee_interaction_graph():
+    # Sample Data
+    elements = {
+        "nodes": [
+            {"data": {"id": 1, "label": "PERSON", "name": "Streamlit"}},
+            {"data": {"id": 2, "label": "PERSON", "name": "Hello"}},
+            {"data": {"id": 3, "label": "PERSON", "name": "World"}},
+            {"data": {"id": 4, "label": "POST", "content": "x"}},
+            {"data": {"id": 5, "label": "POST", "content": "y"}},
+        ],
+        "edges": [
+            {"data": {"id": 6, "label": "FOLLOWS", "source": 1, "target": 2}},
+            {"data": {"id": 7, "label": "FOLLOWS", "source": 2, "target": 3}},
+            {"data": {"id": 8, "label": "POSTED", "source": 3, "target": 4}},
+            {"data": {"id": 9, "label": "POSTED", "source": 1, "target": 5}},
+            {"data": {"id": 10, "label": "QUOTES", "source": 5, "target": 4}},
+        ],
+    }
+
+    # Style node & edge groups
+    node_styles = [
+        NodeStyle("PERSON", "#FF7F3E", "name", "person"),
+        NodeStyle("POST", "#2A629A", "content", "description"),
+    ]
+
+    edge_styles = [
+        EdgeStyle("FOLLOWS", caption='label', directed=True),
+        EdgeStyle("POSTED", caption='label', directed=True),
+        EdgeStyle("QUOTES", caption='label', directed=True),
+    ]
+
+    # Render the component
+    st.markdown("### Employee Interaction Network")
+    st_link_analysis(elements, "cose", node_styles, edge_styles)
+
+selected_graph_view = os.environ["SELECTED_GRAPH_VIEW"] if "SELECTED_GRAPH_VIEW" in os.environ else "Employee Interaction"
+
+if selected_graph_view == "Employee Interaction":
+    employee_interaction_graph()
+elif selected_graph_view == "Project Overview":
+    st.write("Insert graph here")
+
+
+os.environ["SELECTED_GRAPH_VIEW"]= st.selectbox("Choose a Graph View", ["Employee Interaction", "Project Overview"])
